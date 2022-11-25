c920253f0e82f5ca7d167ae0385bef4159213c36
Richard G <richard.guilland@etu.univ-lyon1.fr>
2022-11-25 15:52:20 +0100

    Work: yep

---
+++ src/game/Entity.py
@@ -0,0 +1,9 @@
+class Entity:
+    _position = [0, 0]
+
+    _hp = 0
+    _speed = 0
+    _damage = 0
+    _shoot_speed = 0
+
+
---
+++ src/game/entities/Ship.py
@@ -0,0 +1,4 @@
+from Entity import Entity
+
+class Ship(Entity):
+    def __int__(self):
